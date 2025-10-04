3a6c3c14f11d861cdeacd07e85401c75b34ec878
dudderThugger <adam.szelestey2002@gmail.com>
2025-10-04 18:48:09 +0200

    trying job run

--- script.sh
+++ script.sh
@@ -11,8 +11,6 @@
 
 # Load and install necessary modules
 module purge
-module load 2023
-module load PyTorch/2.1.2-foss-2023a-CUDA-12.1.1
 pip install uv
 
 # Move to project directory CHANGE THIS TO YOUR PROJECT DIRECTORY
@@ -20,12 +18,13 @@ cd $HOME/project/adversarisk-qa
 
 # Creating and activating virtual environment
 uv venv
+uv sync
 source .venv/bin/activate  # activate your virtual environment
 
 # Starting LLM in container
 # define your hugging face token in HF_TOKEN environment variable and hugging face llm model in LLM_MODEL environment variable
 # or add them as an argument for the job submission command: sbatch --export HF_TOKEN=your_token,LLM_MODEL=your_model script.sh
-docker run --runtime nvidia --gpus all     -v ~/.cache/huggingface:/root/.cache/huggingface     --env "HUGGING_FACE_HUB_TOKEN=$HF_TOKEN"     -p 8000:8000     --ipc=host     vllm/vllm-openai:latest     --model "$LLM_MODEL"
+podman run --runtime nvidia --gpus all     -v ~/.cache/huggingface:/root/.cache/huggingface     --env "HUGGING_FACE_HUB_TOKEN=$HF_TOKEN"     -p 8000:8000     --ipc=host     vllm/vllm-openai:latest     --model "$LLM_MODEL"
 
 
 
